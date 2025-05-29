aba1296becf9a44a91c16c5e9b0caff812810876
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-05-28 17:31:52 -0700

    Added auth views to the app routes

--- front_end/src/App.jsx
+++ front_end/src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from "react";
 import { Container } from "@chakra-ui/react";
-import { BrowserRouter } from "react-router"
-import UnauthedViews from "./pageroutes/pageroutes";
+import { BrowserRouter } from "react-router";
+import { UnauthedViews, AuthedViews } from "./pageroutes/pageroutes";
 
 function App() {
   return (
     <Container minHeight="100vh" maxWidth="100%" padding="0" margin="0">
       <BrowserRouter>
         <UnauthedViews></UnauthedViews>
+        <AuthedViews></AuthedViews>
       </BrowserRouter>
     </Container>
   );
