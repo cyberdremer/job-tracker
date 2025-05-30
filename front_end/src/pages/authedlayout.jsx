c05ee5cdd641eedc53b427a13cac5c7a3fffcb17
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-05-30 11:08:34 -0700

    Created layout for authorized pages

---
+++ front_end/src/pages/authedlayout.jsx
@@ -0,0 +1,17 @@
+import { Outlet } from "react-router";
+import { Flex } from "@chakra-ui/react";
+import DashboardHeader from "@/fragments/dashboardheader";
+const AuthedLayout = ({ children }) => {
+  return (
+    <>
+      <Flex grow="1" direction="column" minHeight="100vh">
+        <DashboardHeader></DashboardHeader>
+        <Flex flex="1" direction="column">
+          <Outlet></Outlet>
+        </Flex>
+      </Flex>
+    </>
+  );
+};
+
+export default AuthedLayout;
