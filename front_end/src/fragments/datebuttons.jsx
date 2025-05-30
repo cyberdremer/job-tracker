4dba413de921f607d83e2558410cf316925fce64
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-05-30 11:25:02 -0700

    Created component for filtering by date range

---
+++ front_end/src/fragments/datebuttons.jsx
@@ -0,0 +1,47 @@
+import FilterMenu from "./filtermenu";
+import SwitchControl from "./switch";
+import { VStack, Group, Input, Button } from "@chakra-ui/react";
+
+const DateButtons = ({ startdate, updateDate, endDate, handleRefresh }) => {
+  return (
+    <>
+      <VStack gap={2} alignItems="flex-start">
+        <Input
+          type="date"
+          name="startdate"
+          placeholder="Start Date"
+          value={startdate}
+          onChange={(e) => updateDate}
+          size="sm"
+        ></Input>
+        <Input
+          type="date"
+          name="enddate"
+          placeholder="End Date"
+          value={endDate}
+          onChange={(e) => updateDate}
+          size="sm"
+        ></Input>
+        <Button
+          type="submit"
+          maxW="100%"
+          colorPalette="blue"
+          alignSelf="stretch"
+        >
+          Search for entries
+        </Button>
+        <Button
+          type="submit"
+          maxW="100%"
+          colorPalette="blue"
+          alignSelf="stretch"
+          onClick={handleRefresh}
+        >
+          Reset entries
+        </Button>
+      </VStack>
+    </>
+  );
+};
+
+export default DateButtons;
