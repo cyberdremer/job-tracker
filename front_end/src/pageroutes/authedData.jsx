3f466510bcf107a5b70aef1dfa769cd3c9d2105f
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-05-31 20:02:58 -0700

    Added visualization route

--- front_end/src/pageroutes/authedData.jsx
+++ front_end/src/pageroutes/authedData.jsx
@@ -1,9 +1,18 @@
 import Dashboard from "@/pages/dashboard";
 import Account from "@/pages/account";
+import Visualization from "@/pages/visualization";
 const authedData = [
   { path: "/dashboard", element: <Dashboard></Dashboard>, title: "Dashboard" },
-  { path: "/dashboard/visualization", element: {}, title: "Visualization" },
-  { path: "/dashboard/accountinfo", element: <Account></Account>, title: "Account Info" },
+  {
+    path: "/dashboard/visualizations",
+    element: <Visualization></Visualization>,
+    title: "Visualization",
+  },
+  {
+    path: "/dashboard/accountinfo",
+    element: <Account></Account>,
+    title: "Account Info",
+  },
 ];
 
 export default authedData;
