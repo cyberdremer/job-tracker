2c82c119a874d5d52173501670f4431e5364ae6a
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-06-05 19:32:17 -0700

    Added guard clause to authorized

--- back_end/middleware/authorized.js
+++ back_end/middleware/authorized.js
@@ -1,16 +1,11 @@
 const isAuthorized = (req, res, next) => {
   if (req.isAuthenticated()) {
-    next();
-  } else {
-    res.status(401).json({
-      message: "Unauthorized access",
-      status: 401,
-    });
+    return next();
   }
+  res.status(401).json({
+    message: "Unauthorized access",
+    status: 401,
+  });
 };
-
-
-
-
 
 export default isAuthorized;
