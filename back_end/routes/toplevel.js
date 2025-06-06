042de97f3d7b431ff5a8d98ee6b6f65200db616d
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-06-05 19:30:24 -0700

    Added the job entry route to the top level route

--- back_end/routes/toplevel.js
+++ back_end/routes/toplevel.js
@@ -2,8 +2,9 @@ import { Router } from "express";
 const topLevelRouter = Router();
 import signUpRouter from "./signup.js";
 import loginRouter from "./login.js";
+import entriesRouter from "./jobentries.js";
 
 topLevelRouter.use("/signup", signUpRouter);
 topLevelRouter.use("/login", loginRouter);
-
+topLevelRouter.use("/entry", entriesRouter);
 export default topLevelRouter;
