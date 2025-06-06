43ad4b6c7f677a5cbe1aa2cefa2eb3fc803eb45d
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-06-05 19:22:42 -0700

    Added functioning inputs

--- front_end/src/pages/signup.jsx
+++ front_end/src/pages/signup.jsx
@@ -1,7 +1,17 @@
 import { useState } from "react";
 import { FaGoogle } from "react-icons/fa";
-import { Field, Input, InputGroup, Fieldset, Stack, HStack, Button } from "@chakra-ui/react";
+import {
+  Field,
+  Input,
+  InputGroup,
+  Fieldset,
+  Stack,
+  HStack,
+  Button,
+} from "@chakra-ui/react";
 import timer from "@/utils/popuptimer";
+import { postRequest } from "@/utils/requests";
+import AlertBox from "@/alerts/alertbox";
 const Signup = ({}) => {
   const [form, setForm] = useState({
     firstname: "",
@@ -26,119 +36,153 @@ const Signup = ({}) => {
     setForm({ ...form, [e.target.name]: e.target.value });
   };
 
-  const handleSignUp = (e) => {};
+  const handleSignUp = async (e) => {
+    try {
+      const response = await postRequest("/signup/local", form);
+      if (response.error) {
+        throw new Error(response.error.message);
+      }
+      setSuccess({
+        message: response.data.message,
+        occurred: true,
+      });
 
-  const handleGoogleSignUp = (e) => {
-    
-  }
+      setTimeout(() => {
+        setSuccess({
+          message: "",
+          occurred: false,
+        });
+      }, timer);
+    } catch (error) {
+      setError({
+        message: error.message,
+        occurred: true,
+      });
+
+      setTimeout(() => {
+        setError({
+          message: "",
+          occurred: false,
+        });
+      }, timer);
+    }
+  };
+
+  const handleGoogleSignUp = (e) => {};
 
   return (
     <>
-      {success.occurrred && (
-        <SuccessAlert message={success.message}></SuccessAlert>
+      {success.occurred && (
+        <AlertBox
+          title={"Success"}
+          type={"success"}
+          message={success.message}
+        ></AlertBox>
       )}
-      {error.occurrred && <ErrorAlert message={error.message}></ErrorAlert>}
-     
-        <Fieldset.Root
-          size="lg"
-          maxW="md"
-          alignSelf="center"
-          animationName="fade-in"
-          animationDuration="slowest"
-          marginTop="3rem"
+      {error.occurred && (
+        <AlertBox title="Error" type="error" message={error.message}></AlertBox>
+      )}
+
+      <Fieldset.Root
+        size="lg"
+        maxW="md"
+        alignSelf="center"
+        animationName="fade-in"
+        animationDuration="slowest"
+        marginTop="3rem"
+      >
+        <Stack>
+          <Fieldset.Legend>Sign Up details</Fieldset.Legend>
+          <Fieldset.HelperText>
+            Please enter your sign up details
+          </Fieldset.HelperText>
+        </Stack>
+        <Fieldset.Content>
+          <HStack>
+            <Field.Root required>
+              <Field.Label>
+                First Name:
+                <Field.RequiredIndicator></Field.RequiredIndicator>
+              </Field.Label>
+              <Input
+                name="firstname"
+                value={form.firstname}
+                onChange={handleChange}
+              />
+            </Field.Root>
+            <Field.Root required>
+              <Field.Label>
+                Last Name:
+                <Field.RequiredIndicator></Field.RequiredIndicator>
+              </Field.Label>
+              <Input
+                name="lastname"
+                value={form.lastname}
+                onChange={handleChange}
+              />
+            </Field.Root>
+          </HStack>
+          <Field.Root required>
+            <Field.Label>
+              Email address:
+              <Field.RequiredIndicator></Field.RequiredIndicator>
+            </Field.Label>
+            <Input
+              name="email"
+              type="email"
+              value={form.email}
+              onChange={handleChange}
+            />
+          </Field.Root>
+          <Field.Root required>
+            <Field.Label>
+              Password:
+              <Field.RequiredIndicator></Field.RequiredIndicator>
+            </Field.Label>
+            <Input
+              name="password"
+              type="password"
+              value={form.password}
+              onChange={handleChange}
+            />
+          </Field.Root>
+          <Field.Root required>
+            <Field.Label>
+              Confirm password:
+              <Field.RequiredIndicator></Field.RequiredIndicator>
+            </Field.Label>
+            <Input
+              name="confirmpassword"
+              type="password"
+              value={form.confirmpassword}
+              onChange={handleChange}
+            />
+          </Field.Root>
+          <Field.Root required>
+            <Field.Label>
+              Signup Code: (Case Sensitive)
+              <Field.RequiredIndicator></Field.RequiredIndicator>
+            </Field.Label>
+            <Input
+              name="signupcode"
+              type="input"
+              value={form.signupcode}
+              onChange={handleChange}
+            />
+          </Field.Root>
+        </Fieldset.Content>
+        <Button
+          type="submit"
+          alignSelf="flex-start"
+          minWidth="100%"
+          onClick={handleSignUp}
         >
-          <Stack>
-            <Fieldset.Legend>Sign Up details</Fieldset.Legend>
-            <Fieldset.HelperText>
-              Please enter your sign up details
-            </Fieldset.HelperText>
-          </Stack>
-          <Fieldset.Content>
-            
-            <HStack>
-              <Field.Root required>
-                <Field.Label>
-                  First Name:
-                  <Field.RequiredIndicator></Field.RequiredIndicator>
-                </Field.Label>
-                <Input
-                  name="firstname"
-                  value={form.firstname}
-                  onChange={handleChange}
-                />
-              </Field.Root>
-              <Field.Root required>
-                <Field.Label>
-                  Last Name:
-                  <Field.RequiredIndicator></Field.RequiredIndicator>
-                </Field.Label>
-                <Input
-                  name="lastname"
-                  value={form.lastname}
-                  onChange={handleChange}
-                />
-              </Field.Root>
-            </HStack>
-            <Field.Root required>
-              <Field.Label>
-                Email address:
-                <Field.RequiredIndicator></Field.RequiredIndicator>
-              </Field.Label>
-              <Input
-                name="email"
-                type="email"
-                value={form.email}
-                onChange={handleChange}
-              />
-            </Field.Root>
-            <Field.Root required>
-              <Field.Label>
-                Password:
-                <Field.RequiredIndicator></Field.RequiredIndicator>
-              </Field.Label>
-              <Input
-                name="password"
-                type="password"
-                value={form.password}
-                onChange={handleChange}
-              />
-            </Field.Root>
-            <Field.Root required>
-              <Field.Label>
-                Confirm password:
-                <Field.RequiredIndicator></Field.RequiredIndicator>
-              </Field.Label>
-              <Input
-                name="confirmpassword"
-                type="password"
-                value={form.confirmpassword}
-                onChange={handleChange}
-              />
-            </Field.Root>
-            <Field.Root required>
-              <Field.Label>
-                Signup Code: (Case Sensitive)
-                <Field.RequiredIndicator></Field.RequiredIndicator>
-              </Field.Label>
-              <Input
-                name="signupcode"
-                type="input"
-                value={form.signupcode}
-                onChange={handleChange}
-              />
-            </Field.Root>
-          </Fieldset.Content>
-          <Button
-            type="submit"
-            alignSelf="flex-start"
-            minWidth="100%"
-            onClick={handleSignUp}
-          >
-            Create account
-          </Button>
-          <Button>Sign Up With <FaGoogle></FaGoogle> </Button>
-        </Fieldset.Root>
-     
+          Create account
+        </Button>
+        <Button>
+          Sign Up With <FaGoogle></FaGoogle>{" "}
+        </Button>
+      </Fieldset.Root>
     </>
   );
 };
