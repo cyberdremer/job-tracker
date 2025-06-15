c055c59e06aed28295d3b1bfad8d2ecea2d46970
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-06-14 21:02:24 -0700

    Added check for environment to trust proxy in prod

--- back_end/app.js
+++ back_end/app.js
@@ -12,10 +12,13 @@ const corsConfig = cors({
     process.env.NODE_ENV === "dev"
       ? "http://localhost:5173"
       : process.env.ORIGIN_URL,
-  methods: ["GET", "POST", "DELETE", "OPTIONS"],
+  methods: ["GET", "POST", "DELETE", "OPTIONS", "PUT"],
 });
 
 const app = express();
+if (process.env.NODE_ENV === "prod") {
+  app.set("trust proxy", 1);
+}
 app.use(corsConfig);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
