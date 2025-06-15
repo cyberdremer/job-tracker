22929d17964b964d9641fc91462f95d01f999e0e
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-06-14 20:58:34 -0700

    Added routes for signing up

--- back_end/routes/signup.js
+++ back_end/routes/signup.js
@@ -1,8 +1,8 @@
 import { Router } from "express";
 const signUpRouter = Router();
-import { signUpController } from "../controllers/signup.js";
+import { signUpController, googleSignUpController} from "../controllers/signup.js";
 
 signUpRouter.post("/local", signUpController);
-signUpRouter.post("/google", () => {});
+
 
 export default signUpRouter;
