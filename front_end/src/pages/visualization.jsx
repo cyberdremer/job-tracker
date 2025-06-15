5b6a984599524de71837022e57b1d151a2bab272
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-06-14 20:31:16 -0700

    Finished visualization

--- front_end/src/pages/visualization.jsx
+++ front_end/src/pages/visualization.jsx
@@ -6,17 +6,64 @@ import {
 import mockJobEntries from "@/mockdata/mockjobentries";
 import { Box, Heading, Flex, VStack } from "@chakra-ui/react";
 import DateButtons from "@/fragments/datebuttons";
-import { useState } from "react";
+import { useEffect, useMemo, useState } from "react";
+import { useEntriesHook } from "@/effects/hooks";
+import { protectedGetRequest } from "@/utils/requests";
+import LoadingPlaceholder from "@/fragments/loading";
+import EmptyContainer from "@/fragments/emptycontainer";
 const Visualization = () => {
-  const [dateForm, setDateForm] = useState({
-    startdate: "",
-    enddate: "",
+  const { entries, setEntries, fetchError, loading } = useEntriesHook(
+    "/entry/retrieve/all"
+  );
+
+  const [searchToggled, setSearchToggled] = useState({
+    toggled: false,
+    message: "",
   });
 
-  const handleRefresh = () => {};
+  const [filter, setFilter] = useState({
+    dateRange: "",
+  });
+  const [success, setSuccess] = useState({
+    message: "",
+    status: false,
+  });
 
-  const handleChange = (e) => {
-    setDateForm({ ...dateForm, [e.target.name]: e.target.value });
+  const [error, setError] = useState({
+    message: "",
+    status: false,
+  });
+
+  const filteredEntries = useMemo(() => {
+    let result = [...entries];
+    if (filter.dateRange) {
+      const startDate = new Date(filter.dateRange.startDate);
+      const endDate = new Date(filter.dateRange.endDate);
+      setSearchToggled({
+        ...searchToggled,
+        toggled: true,
+        message: `Showing results from ${startDate.toLocaleDateString()} to ${endDate.toLocaleDateString()}`,
+      });
+      result = result.filter((entry) => {
+        return !(
+          new Date(entry.dateapplied).toLocaleDateString() >=
+            startDate.toLocaleDateString() &&
+          new Date(entry.dateapplied).toLocaleDateString() <=
+            endDate.toLocaleDateString()
+        );
+      });
+    }
+
+    return result;
+  }, [entries, filter]);
+
+  const handleDateRange = (startDate, endDate) => {
+    setFilter((f) => ({ ...f, dateRange: { startDate, endDate } }));
+  };
+
+  const handleRefresh = () => {
+    setSearchToggled({ ...form, toggled: false, message: "" });
+    setFilter({ dateRange: null });
   };
 
   return (
@@ -25,31 +72,53 @@ const Visualization = () => {
         <Box minW="220px" maxW="260px">
           <VStack align="stretch" gap={4}>
             <DateButtons
-              startdate={dateForm.startdate}
-              endDate={dateForm.enddate}
               handleRefresh={handleRefresh}
-              updateDate={handleChange}
+              searchForDate={handleDateRange}
             ></DateButtons>
           </VStack>
         </Box>
-        <Box flex={1}>
-          <Heading textAlign="center">
-            Job Hunt for {new Date().toLocaleDateString()}
-          </Heading>
-          <Box width="100%" height="400px" marginTop={4}>
-            <JobEntryBarChart jobEntries={mockJobEntries} />
-          </Box>
+        <Box flex="1">
+          {loading && <LoadingPlaceholder></LoadingPlaceholder>}
+          {fetchError && (
+            <EmptyContainer
+              message={"Error in loading entries, please try again later"}
+            ></EmptyContainer>
+          )}
+          {(() => {
+            if (!loading && !fetchError) {
+              if (entries.length > 0) {
+                return (
+                  <>
+                    <Heading textAlign="center">
+                      {searchToggled.toggled
+                        ? searchToggled.message
+                        : "All Time Job Entries Visualization"}
+                    </Heading>
+                    <Box width="100%" height="400px" marginTop={4}>
+                      <JobEntryBarChart jobEntries={filteredEntries} />
+                    </Box>
 
-          <Box
-            width="100%"
-            height="400px"
-            marginTop={4}
-            display="flex"
-            justifyContent="center"
-            alignItems="center"
-          >
-            <JobEntryPieChart jobEntries={mockJobEntries} />
-          </Box>
+                    <Box
+                      width="100%"
+                      height="400px"
+                      marginTop={4}
+                      display="flex"
+                      justifyContent="center"
+                      alignItems="center"
+                    >
+                      <JobEntryPieChart jobEntries={filteredEntries} />
+                    </Box>
+                  </>
+                );
+              } else {
+                return (
+                  <EmptyContainer
+                    message={"No job entries found, please add some!"}
+                  ></EmptyContainer>
+                );
+              }
+            }
+          })()}
         </Box>
       </Flex>
     </>
