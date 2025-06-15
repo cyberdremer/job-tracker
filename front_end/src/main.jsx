bbe4ab98501e6986c894382612f8c5e4596c2ebb
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-06-14 18:15:15 -0700

    Added auth and info providers

--- front_end/src/main.jsx
+++ front_end/src/main.jsx
@@ -1,13 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
+import { AuthProvider } from "./context/authcontext";
+import { InfoProvider } from "./context/infocontext";
 import { Provider } from "./components/ui/provider";
 import App from "./App";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider>
-      <App />
+      <InfoProvider>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </InfoProvider>
     </Provider>
   </StrictMode>
 );
