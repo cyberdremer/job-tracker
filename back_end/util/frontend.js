a9b1b6219af798a437546d83021ae8897db717dd
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-06-14 21:00:06 -0700

    Added module for when the server needs to redirect the frontend on oauth login

---
+++ back_end/util/frontend.js
@@ -0,0 +1,12 @@
+import "dotenv/config";
+
+const frontendUrl =
+  process.env.NODE_ENV === "dev"
+    ? process.env.LOCAL_FRONTEND_URL
+    : `${
+        process.PRODUCTION_FRONTEND_URL +
+        ":" +
+        process.env.PRODUCTION_FRONTEND_PORT
+      }`;
+
+export default frontendUrl;
