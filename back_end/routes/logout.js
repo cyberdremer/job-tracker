af03027a57f84ea7f06a3d56a7719a1e7eb94289
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-06-14 20:58:05 -0700

    Added route for logging out

---
+++ back_end/routes/logout.js
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import logoutController from "../controllers/logout.js";
+const logoutRouter = Router();
+
+logoutRouter.post("/", logoutController);
+
+
+export default logoutRouter
