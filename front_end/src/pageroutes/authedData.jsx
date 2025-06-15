53d281b0bfcac9f948cb8d7557eec3d5a23e94d4
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-06-14 20:38:02 -0700

    Created route data

--- front_end/src/pageroutes/authedData.jsx
+++ front_end/src/pageroutes/authedData.jsx
@@ -1,16 +1,33 @@
 import Dashboard from "@/pages/dashboard";
 import Account from "@/pages/account";
 import Visualization from "@/pages/visualization";
+import RequireAuth from "./protection";
 const authedData = [
-  { path: "/dashboard", element: <Dashboard></Dashboard>, title: "Dashboard" },
+  {
+    path: "/dashboard",
+    element: (
+      <RequireAuth>
+        <Dashboard></Dashboard>
+      </RequireAuth>
+    ),
+    title: "Dashboard",
+  },
   {
     path: "/dashboard/visualizations",
-    element: <Visualization></Visualization>,
+    element: (
+      <RequireAuth>
+        <Visualization></Visualization>
+      </RequireAuth>
+    ),
     title: "Visualization",
   },
   {
     path: "/dashboard/accountinfo",
-    element: <Account></Account>,
+    element: (
+      <RequireAuth>
+        <Account></Account>
+      </RequireAuth>
+    ),
     title: "Account Info",
   },
 ];
--- front_end/src/pageroutes/unauthedData.jsx
+++ front_end/src/pageroutes/unauthedData.jsx
@@ -1,12 +1,12 @@
 import Login from "@/pages/login";
 import SignUp from "@/pages/signup";
 import Landing from "@/pages/landing";
+import LogOut from "@/pages/logout";
 const unAuthedData = [
   { path: "/login", element: <Login></Login>, title: "Login" },
   { path: "/signup", element: <SignUp></SignUp>, title: "Signup" },
   { path: "/", element: <Landing></Landing>, title: "Landing Page" },
+  { path: "/logout", element: <LogOut></LogOut>, title: "Log Out" },
 ];
-
-
 
 export default unAuthedData;
