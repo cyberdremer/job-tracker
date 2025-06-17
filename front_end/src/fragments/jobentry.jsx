40181ca3ba9da1d15638bd1ca224308eaceda84d
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-06-16 17:31:09 -0700

    Switched buttons for icon buttons

--- front_end/src/fragments/jobentry.jsx
+++ front_end/src/fragments/jobentry.jsx
@@ -7,8 +7,11 @@ import {
   Badge,
   Text,
   FormatNumber,
+  IconButton,
 } from "@chakra-ui/react";
 import { Link } from "@chakra-ui/react";
+import { FaEdit } from "react-icons/fa";
+import { FaDeleteLeft, FaTrash } from "react-icons/fa6";
 import Tilt from "react-parallax-tilt";
 
 const statusColor = {
@@ -34,72 +37,77 @@ const JobEntryCards = ({
   dateapplied,
 }) => {
   return (
-    
-      <Card.Root
-        width="350px"
-        minHeight="180px"
-        margin="4"
-        boxShadow="md"
-        borderRadius="lg"
-        padding="6"
-      >
-        <Tilt>
-          <Card.Header>
-            <HStack justifyContent="space-between" alignItems="center">
-              <Heading size="md">
-                {link === "" ? (
-                  <>
-                    <Text>{title}</Text>
-                  </>
-                ) : (
-                  <Link target="_blank" href={link}>
-                    {title}
-                  </Link>
-                )}
-              </Heading>
-              <Badge colorPalette={statusColor[status?.toUpperCase()] || "gray"}>
-                {status}
-              </Badge>
-            </HStack>
-            <Text fontSize="sm" color="gray.500">
+    <Card.Root
+      width="350px"
+      minHeight="180px"
+      margin="4"
+      boxShadow="md"
+      borderRadius="lg"
+      padding="6"
+    >
+      <Tilt>
+        <Card.Header>
+          <HStack justifyContent="space-between" alignItems="center">
+            <Heading size="md">
+              {link === "" ? (
+                <>
+                  <Text>{title}</Text>
+                </>
+              ) : (
+                <Link target="_blank" href={link}>
+                  {title}
+                </Link>
+              )}
+            </Heading>
+            <Badge colorPalette={statusColor[status?.toUpperCase()] || "gray"}>
+              {status}
+            </Badge>
+          </HStack>
+          <Text fontSize="sm" color="gray.500">
+            {company}
+          </Text>
+        </Card.Header>
+        <Card.Body>
+          <Stack spacing="1">
+            <Text>
+              <b>Company: </b>
               {company}
             </Text>
-          </Card.Header>
-          <Card.Body>
-            <Stack spacing="1">
-              <Text>
-                <b>Company: </b>
-                {company}
-              </Text>
-              <Text>
-                <b>Location:</b> {location}
-              </Text>
-              <Text>
-                <b>Salary:</b>{" "}
-                <FormatNumber
-                  value={salary}
-                  style="currency"
-                  currency="USD"
-                ></FormatNumber>
-              </Text>
-              <Text>
-                <b>Date Applied:</b> {dateapplied}
-              </Text>
-            </Stack>
-          </Card.Body>
-          <Card.Footer>
-            <HStack spacing="3">
-              <Button size="sm" colorScheme="blue" onClick={() => onEdit()}>
-                Edit
-              </Button>
-              <Button size="sm" colorScheme="red" onClick={() => onDelete()}>
-                Delete
-              </Button>
-            </HStack>
-          </Card.Footer>
-        </Tilt>
-      </Card.Root>
-    
+            <Text>
+              <b>Location:</b> {location}
+            </Text>
+            <Text>
+              <b>Salary:</b>{" "}
+              <FormatNumber
+                value={salary}
+                style="currency"
+                currency="USD"
+              ></FormatNumber>
+            </Text>
+            <Text>
+              <b>Date Applied:</b> {dateapplied}
+            </Text>
+          </Stack>
+        </Card.Body>
+        <Card.Footer>
+          <HStack spacing="3">
+            <IconButton size="sm" colorScheme="blue" onClick={() => onEdit()}>
+              <FaEdit></FaEdit>
+            </IconButton>
+
+          
+
+            <IconButton
+              size={"sm"}
+              colorScheme="red"
+              onClick={() => onDelete()}
+            >
+              <FaTrash></FaTrash>
+            </IconButton>
+          </HStack>
+        </Card.Footer>
+      </Tilt>
+    </Card.Root>
   );
 };
 
