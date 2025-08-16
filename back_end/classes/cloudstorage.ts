95002437a3a147d337dcc9d59bc0a31492e5390b
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-08-15 19:55:41 -0700

    Added a class for managing different cloud providers

---
+++ back_end/classes/cloudstorage.ts
@@ -0,0 +1,21 @@
+import { CloudStorageStrategy } from "../interfaces/cloud";
+
+export class CloudStorage {
+  private strategy: CloudStorageStrategy;
+
+  constructor(strategy: CloudStorageStrategy) {
+    this.strategy = strategy;
+  }
+
+  setStrategy(strategy: CloudStorage) {
+    this.strategy = strategy;
+  }
+
+  async uploadFile(file: Buffer, publicId: string) {
+    return this.strategy.uploadFile(file, publicId);
+  }
+
+  async deleteFile(publicId: string) {
+    return this.strategy.deleteFile(publicId);
+  }
+}
