d50e8cac2a00077274184270983886bcbae83c04
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-08-22 19:47:50 -0700

    Added pagination params to Express Request object

--- back_end/src/interfaces/index.d.ts
+++ back_end/src/interfaces/index.d.ts
@@ -1,8 +1,10 @@
 import { DeserializedUser } from "./user.ts"
+import { PaginationOptions, PaginationParams } from "./pagination.ts"
 declare global {
     namespace Express {
         export interface Request{
-            user: DeserializedUser 
+            user: DeserializedUser
+            pagination?: PaginationParams 
         }
     }
 }
