953c9fe6073b3fbbfd7c92391c337632bf200a44
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-08-22 19:53:53 -0700

    Created filtered types for responses

--- back_end/src/interfaces/serverresponses.ts
+++ back_end/src/interfaces/serverresponses.ts
@@ -1,7 +1,37 @@
+import { ResumeJobFeedback, Status } from "@prisma/client";
 export interface SuccessfullServerResponse<T = undefined> {
-    data: {
-        message: string;
-        status: number;
-        object?: T
-    }
+  data: {
+    message: string;
+    status: number;
+    object?: T;
+  };
 }
+
+export type JobEntryFiltered = {
+  id: number;
+  title: string;
+  company: string;
+  location: string;
+  salary: string;
+  updatedAt: Date;
+  dateApplied: Date;
+  status: Status;
+};
+
+export type ResumeJobFiltered = {
+  id: number;
+  rating: number;
+  name: string;
+
+};
+
+export type ResumeFiltered = {
+  id: number;
+  cloudinaryLink: string;
+  cloudinaryPublicId: string;
+  uploadedAt: Date;
+  lastModified: Date;
+};
+
+
+
